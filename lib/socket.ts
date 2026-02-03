7ef5e48e2dfcc9109f752a0aee85e9c1a81f93bc
Yam Guragain <yamguragain30@gmail.com>
2026-02-03 18:07:34 +0545

    websocket fixed

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { getToken } from "@/lib/auth";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
   autoConnect: false,
   transports: ["websocket"], // important
   auth: {
